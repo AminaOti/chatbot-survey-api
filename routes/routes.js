fa20b8d6a020627b81554f49054c0cf0f1002431
Amina Oti <aminaoti@Aminas-MacBook-Pro.local>
2023-12-22 15:41:42 +0000

    feat: tidy up route file

--- routes/routes.js
+++ routes/routes.js
@@ -3,19 +3,12 @@ const router = express.Router();
 const {
   sendMessageToLmmController,
 } = require("../controllers/sendMessageToLmmController");
-//const sendMessageToLlm = require("../services/sendMessageToLlmServices");
-
-const BEARER_TOKEN = process.env.LLM_BEARER_TOKEN;
-const MODEL_ID = process.env.LLM_MODEL_ID;
-const URL = process.env.LLM_URL; //"https://bam-api.res.ibm.com/v2/text/chat";
 
 router.get("/", (req, res) => {
-  res
-    .status(200)
-    .json({
-      success: "true",
-      message: "congratulations, you have hit the endpoint for the chatbotApi",
-    });
+  res.status(200).json({
+    success: "true",
+    message: "congratulations, you have hit the endpoint for the chatbotApi",
+  });
 });
 
 router.route("/sendMessage").post(sendMessageToLmmController);
